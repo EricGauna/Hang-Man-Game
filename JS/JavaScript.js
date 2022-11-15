374f73504c29f8f75e5cc439da6f8b9fbaed03c2
Abrahambp1994 <abrahambp1994@icloud.com>
2022-11-15 01:51:12 +0100

    Array con los países y las comidas

---
+++ JS/JavaScript.js
@@ -0,0 +1,24 @@
+const palabras = [
+    {nombre: "TORTILLA",
+    pista: "comida"},
+    {nombre: "MACARRONES",
+    pista: "comida"},
+    {nombre: "COCIDO",
+    pista: "comida"},
+    {nombre: "PAELLA",
+    pista: "comida"},
+    {nombre: "HAMBURGUESA",
+    pista: "comida"},
+    {nombre: "ESPAÑA",
+    pista: "pais"},
+    {nombre: "FRANCIA",
+    pista: "pais"},
+    {nombre: "MEXICO",
+    pista: "pais"},
+    {nombre: "ARGENTINA",
+    pista: "pais"},
+    {nombre: "CHINA",
+    pista: "pais"},
+]
+
+console.log(palabras);
